e3ae7d5a282692d0ef287b00583d22c7ba9b5561
Matheus Barros Lima <matheusmatheusbarroslima6@gmail.com>
2024-03-18 16:14:30 -0300

    add reset and theme project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from "./pages/App"
+import { Reset } from './styles/reset'
+import { ThemeProvider } from 'styled-components'
+import { appTheme } from './styles/theme'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ThemeProvider theme={appTheme}>
     <App />
+    <Reset/>
+    </ThemeProvider>
   </React.StrictMode>,
 )
