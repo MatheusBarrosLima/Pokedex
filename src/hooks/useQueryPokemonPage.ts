dd8f6dd4c4200b6f652f8d9c9a79338410f201e3
Matheus Barros Lima <matheusmatheusbarroslima6@gmail.com>
2024-03-27 16:01:43 -0300

    add navigation and validation routes params and query params

--- src/hooks/useQueryPokemonPage.ts
+++ src/hooks/useQueryPokemonPage.ts
@@ -1,12 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { API } from "../configs/api";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Pokemon } from "../@types/pokemon";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export function useQueryPokemonPage() {
    const [page, setPage] = useState(1) ;
    const [limit, setLimit] = useState(30); 
    const [totalPages, setTotalPages] = useState(0);
+
+   const navigate = useNavigate();
+   const searchParams = useSearchParams()
 
    async function getPokemonPage({page = 1, limit = 30}) {
        if (page <= 0) page = 1;
@@ -28,6 +32,38 @@ export function useQueryPokemonPage() {
        return pokemonData as Pokemon[];
     } 
 
+    function nextPage() {
+        if (page < totalPages) {
+            setPage((value) => value + 1);
+            navigate( `?page=${page + 1}`)
+        }
+    }
+
+    function prevPage() {
+        if (page > 1) {
+            setPage((value) => value - 1);
+            navigate( `?page=${page - 1}`)
+        }
+    }
+
+    useEffect(() => {
+       const pageQuery = Number (searchParams[0].get("page"));
+       setPage(pageQuery || 1);
+
+       if (totalPages > 0) {
+        if (pageQuery > totalPages) {
+            navigate(`?page=${totalPages}`)
+            setPage(totalPages)
+            return
+        }
+
+        if (pageQuery < 1) {
+            navigate(`?page=1`)
+            setPage(1)
+            return
+        }
+       }
+    }, [page, navigate, searchParams, totalPages])
 
     const query = useQuery({
         queryKey: [`getPokemonPage${page}-${limit}`, page, limit],
@@ -38,5 +74,8 @@ export function useQueryPokemonPage() {
         ...query,
         page,
         totalPages,
+        nextPage,
+        prevPage,
+        setLimit,
     };
 }
